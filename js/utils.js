2e11831ab34ad04add53b0fda45a40a5b832f49e
Nurkhayot <nurxayotmansurov@gmail.com>
2021-07-31 14:42:21 +0500

    joylar o'zgardi

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,10 @@
 const creator = (creatingElement) => document.createElement(creatingElement);
-const setter = (gettingElement) => document.querySelector(gettingElement)
+const setter = (gettingElement) => document.querySelector(gettingElement);
 
-let makeTime = (time => {
-    const day = String(new Date(time).getDate()).padStart(2, '0');
-    const month = String(new Date(time).getMonth() + 1).padStart(2, '0');
-    const year = new Date(time).getFullYear();
+let makeTime = (time) => {
+  const day = String(new Date(time).getDate()).padStart(2, "0");
+  const month = String(new Date(time).getMonth() + 1).padStart(2, "0");
+  const year = new Date(time).getFullYear();
 
-    return day + '.' + month + '.' + year
-})
+  return day + "." + month + "." + year;
+};
